5d427594b4f2f94ff39989b0210901eb5fc56414
chideramodili <115737267+chideramodili@users.noreply.github.com>
2023-02-12 21:30:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-/*const http = require("http");
+const http = require("http");
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('content.type', 'text/html')
+ 
+
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`server running on ${PORT}`));*/
+server.listen(PORT, () => console.log(`server running on ${PORT}`));
 
 const rooms = require("./rooms");
 
@@ -40,61 +40,16 @@ function deleteroom(rooms, deleteId) {
 
 async function myRoom() {
   try {
-    //let theRoom = await getRoom();
-    //let specificRoom = await getSpecificRoom(8);
+    let theRoom = await getRoom();
+    let specificRoom = await getSpecificRoom(8);
     let deleted = deleteroom(rooms, 6);
   } catch (error) {
     console.log("error");
+    console.log(error);
   }
 }
 myRoom();
 
-/*function getRoom(email, password) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(`here is your data`);
-      resolve({ userEmail: email });
-    }, 3000);
-  });
-}
-function getVideo(email) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(["vid 1", "vid 2", "vid 3", "vid 4"]);
-      resolve(["vid 1", "vid 2", "vid 3", "vid 4"]);
-    }, 5000);
-  });
-}
-async function mom() {
-  try {
-    let myRoom = await getRoom("dandelion@gmail.com", 123456789);
-    let myVid = await getVideo(getRoom.userEmail);
-  } catch (error) {
-    console.log("error ");
-  }
-}
-mom();
-/*const user = getRoom("dandelion@gmail.com", 123456, user => {
 
-  console.log(user);
-  getVideo(user.userEmail, videos => {
-    console.log(videos );
-  });
-});
 
-console.log("finish");
-//console.log(room);
-
-/*let promise = new Promise(resolve => {
-  setTimeout(() => {
-    console.log("get the user");
-    resolve(["vid 1", "vid 2", "vid 3"]);
-  }, 5000);
-});
-let my = new Promise(resolve => {
-  setTimeout(() => {
-    console.log("get the user");
-    resolve({ user: "eddi" });
-  }, 5000);
-});
-Promise.all([promise, my]).then(result => console.log(result));*/
+    
